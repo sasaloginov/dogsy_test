a9235b4ecc29234c5fb2aa481e26e887e45f7ea4
sasaloginov <sasaloginov@gmail.com>
2019-04-12 13:14:07 +0500

    fix

--- components/FileParser.php
+++ components/FileParser.php
@@ -23,4 +23,41 @@ class FileParser
 
         return $count;
     }
+
+    /**
+     * @param $fileIn
+     * @param $fileOut
+     * @param $regExpFindDate
+     * @param string $dateFormatReplace
+     * @return int
+     */
+    protected static function copyFileWithReplaceDates($fileIn, $fileOut, $regExpFindDate, $dateFormatReplace = 'm-d-Y')
+    {
+        $handleIn = fopen($fileIn, "r");
+        $handleOut = fopen($fileOut, "w");
+        $totalCount = 0;
+        while (!feof($handleIn)) {
+            $bufer = fgets($handleIn);
+            if (false !== preg_match_all($regExpFindDate, $bufer, $matches)) {
+                foreach ( $matches[0] as $date) {
+                    $dDigs = explode('/', $date);
+                    $d = $dDigs[0];
+                    $m = $dDigs[1];
+                    $y = $dDigs[2];
+
+                    if (false === $time = strtotime($m.'/'.$d.'/'.$y)) {
+                        continue;
+                    }
+                    $replacementDate = date($dateFormatReplace, $time);
+                    $bufer = str_replace($date, $replacementDate, $bufer, $count);
+                    $totalCount += $count;
+                }
+                fputs($handleOut, $bufer);
+            }
+        }
+        fclose($handleIn);
+        fclose($handleOut);
+
+        return $totalCount;
+    }
 }
