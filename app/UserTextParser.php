9c2795b6f548d65918295e81d16c7e578b94aa2a
sasaloginov <sasaloginov@gmail.com>
2019-04-12 13:04:00 +0500

    got it

--- app/UserTextParser.php
+++ app/UserTextParser.php
@@ -160,26 +160,6 @@ class UserTextParser extends FileParser implements UserTextParserInterface
     }
 
     /**
-     * Считает количество срок в файле
-     *
-     * @param $file
-     * @return int
-     */
-    protected static function countLines($file)
-    {
-        $handle = fopen($file, "r");
-        $count = 0;
-        while (!feof($handle)) {
-            $bufer = fread($handle, 1048576);
-            $count += substr_count($bufer, "\n");
-        }
-        fclose($handle);
-        $count++;
-
-        return $count;
-    }
-
-    /**
      * @param $fileIn
      * @param $fileOut
      * @param $regExpFindDate
--- components/FileParser.php
+++ components/FileParser.php
@@ -4,5 +4,23 @@ namespace components;
 
 class FileParser
 {
+    /**
+     * Считает количество срок в файле
+     *
+     * @param $file
+     * @return int
+     */
+    protected static function countLines($file)
+    {
+        $handle = fopen($file, "r");
+        $count = 0;
+        while (!feof($handle)) {
+            $bufer = fread($handle, 1048576);
+            $count += substr_count($bufer, "\n");
+        }
+        fclose($handle);
+        $count++;
 
+        return $count;
+    }
 }
